5df0519e27a5c425f7f948ccb904b5770acee69d
qabdullahi <qauom.abdullahi@gmail.com>
2023-01-28 22:45:50 +0100

    New Branch 2

--- src/test/java/Test3.java
+++ src/test/java/Test3.java
@@ -1,5 +1,7 @@
 public class Test3 {
     public static void main(String[] args) {
         System.out.println("Test3");
+        System.out.println("Branch 1");
+        System.out.println("Branch 2");
     }
 }
